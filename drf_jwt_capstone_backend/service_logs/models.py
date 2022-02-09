4b745cc5bef61e8c629bf0f9bdacf109c296b179
TPavZ <tmpavlic@gmail.com>
2022-02-09 15:04:06 -0600

    chore: started service model.

--- drf_jwt_capstone_backend/service_logs/models.py
+++ drf_jwt_capstone_backend/service_logs/models.py
@@ -1,6 +1,12 @@
 from django.db import models
-from django.contrib.auth import get_user_model   
+from django.contrib.auth import get_user_model  
 User=get_user_model()
 # Create your models here.
 class Service(models.Model):
-    pass
+    user = models.ForeignKey(User,null=True, blank=True, on_delete=models.CASCADE)
+    vehicle = models.ForeignKey('vehicles.Vehicle', null=True, blank=True, on_delete=models.SET_NULL) #TODO Somehow be able to connect to the users vehicle.
+    shop = models.ForeignKey('shops.Shop', null=True, blank=True, on_delete=models.SET_NULL) #TODO Somehow be able to connect to the shop work was done at.
+    service_date = models.DateField(auto_now=True)
+    service_grand_total = models.DecimalField(max_digits=7, default=0.0, decimal_places=2)
+
+    details = models.TextField(blank=True, null=True)
